21fd08f9f1e4ec44af0558c4420d0b3a38cad575
NabilaFerdousPrapty <128033656+NabilaFerdousPrapty@users.noreply.github.com>
2024-06-02 23:11:29 +0600

    navbar added

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,11 +1,21 @@
+import { Children } from 'react';
+import MainLayout from './../Layouts/MainLayout';
 import {
+
     createBrowserRouter,
     
   } from "react-router-dom";
+import Home from '../Pages/Home/Home';
 
  export const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <MainLayout/>,
+      Children:[
+        {
+          path:'/',
+          element:<Home/>
+        }
+      ]
     },
   ]);
