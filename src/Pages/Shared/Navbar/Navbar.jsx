01dbf8eb115decfdd43001aeefc0fbcce87e2eeb
NabilaFerdousPrapty <128033656+NabilaFerdousPrapty@users.noreply.github.com>
2024-06-03 23:45:48 +0600

    edits

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import  { useState } from 'react';
 import img from '../../../assets/img.png'
 import { Link, NavLink } from 'react-router-dom';
+import { AiOutlineLogin } from "react-icons/ai";
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
         <nav className="relative  shadow   rounded-3xl">
-            <div className="container px-6 py-4 mx-auto">
+            <div className="container px-6 py-1 mx-auto">
                 <div className="lg:flex lg:items-center lg:justify-between">
                     <div className="flex items-center justify-between">
                         <Link to={'/'}>
@@ -42,28 +43,33 @@ const Navbar = () => {
                     >
                         <div className="flex flex-col -mx-6 lg:flex-row lg:items-center lg:mx-8">
                             <NavLink to={'/'} className="px-3 py-2 mx-3 mt-2 transition-colors duration-300 transform rounded-md lg:mt-0 \ hover:bg-gray-100 dark:hover:bg-gray-700">Home</NavLink>
-                            <a href="#" className="px-3 py-2 mx-3 mt-2  transition-colors duration-300 transform rounded-md lg:mt-0  hover:bg-gray-100 dark:hover:bg-gray-700">Browse Topics</a>
-                            <a href="#" className="px-3 py-2 mx-3 mt-2 transition-colors duration-300 transform rounded-md lg:mt-0  hover:bg-gray-100 dark:hover:bg-gray-700">Random Item</a>
-                            <a href="#" className="px-3 py-2 mx-3 mt-2 transition-colors duration-300 transform rounded-md lg:mt-0  hover:bg-gray-100 dark:hover:bg-gray-700">Experts</a>
+                            <NavLink to={'/'} className="px-3 py-2 mx-3 mt-2 transition-colors duration-300 transform rounded-md lg:mt-0 \ hover:bg-gray-100 dark:hover:bg-gray-700">
+                            All Trainer
+                            </NavLink>
+                            <NavLink to={'/'} className="px-3 py-2 mx-3 mt-2 transition-colors duration-300 transform rounded-md lg:mt-0 \ hover:bg-gray-100 dark:hover:bg-gray-700">
+                            All Classes
+                            </NavLink>
+                            <NavLink to={'/'} className="px-3 py-2 mx-3 mt-2 transition-colors duration-300 transform rounded-md lg:mt-0 \ hover:bg-gray-100 dark:hover:bg-gray-700">
+                            Dashboard
+                            </NavLink>
+                            <NavLink to={'/'} className="px-3 py-2 mx-3 mt-2 transition-colors duration-300 transform rounded-md lg:mt-0 \ hover:bg-gray-100 dark:hover:bg-gray-700">
+                            Community
+                            </NavLink>
+                           
+                            
+                            
                         </div>
 
                         <div className="flex items-center mt-4 lg:mt-0">
-                            <button
-                                className="hidden mx-4 text-[#17ACAC] transition-colors duration-300 transform lg:block dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-400 focus:text-gray-700 dark:focus:text-gray-400 focus:outline-none"
-                                aria-label="show notifications"
-                            >
-                                <svg className="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                                </svg>
-                            </button>
-
-                            <button type="button" className="flex items-center focus:outline-none" aria-label="toggle profile dropdown">
+                           
+                           
+                            {/* <button type="button" className="flex items-center focus:outline-none" aria-label="toggle profile dropdown">
                                 <div className="w-8 h-8 overflow-hidden border-2 border-gray-400 rounded-full">
                                     <img src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" className="object-cover w-full h-full" alt="avatar" />
                                 </div>
 
                                 <h3 className="mx-2 text-gray-700 dark:text-gray-200 lg:hidden">Khatab wedaa</h3>
-                            </button>
+                            </button> */}
                         </div>
                     </div>
                 </div>
