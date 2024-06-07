260827d7af87fe373e8fff18c4b5bb38522260c1
NabilaFerdousPrapty <128033656+NabilaFerdousPrapty@users.noreply.github.com>
2024-06-08 01:44:39 +0600

    trainer details page done

--- src/Pages/AllTrainers/TrainerDetails/TrainerDetails.jsx
+++ src/Pages/AllTrainers/TrainerDetails/TrainerDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import UseAxiosCommon from "../../../hooks/UseAxiosCommon";
 
 const TrainerDetails = () => {
@@ -19,17 +19,123 @@ const TrainerDetails = () => {
 
     fetchTrainer();
   }, [axiosCommon, id]);
-
+  console.log(trainer);
   return (
     <div>
       {trainer ? (
-        <div>
-          <h2>{trainer.name}</h2>
-          <p>{trainer.designation}</p>
-        
+        <div className="grid grid-cols-1 lg:grid-cols-4 p-5 my-4">
+          <div className="max-w-2xl overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800 col-span-3">
+            <img
+              className="object-cover w-full h-screen"
+              src={trainer.profile_image}
+              alt="Article"
+            />
+
+            <div className="p-6">
+              <div>
+                <span className="text-xs font-medium text-blue-600 uppercase dark:text-blue-400">
+                 {
+                    trainer.designation
+                 }
+                </span>
+                <a
+                  href="#"
+                  className="block mt-2 text-xl font-semibold text-gray-800 transition-colors duration-300 transform dark:text-white hover:text-gray-600 hover:underline"
+                  tabindex="0"
+                  role="link"
+                >
+                 {
+                    trainer.other_info
+                 }
+                </a>
+                <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
+                  {trainer.bio}
+                </p>
+                <p className="mt-2 text-sm text-gray-600 dark:text-gray-400 font-merriweather font-semibold text-center">
+                  {trainer.background_and_qualifications}
+                </p>
+              </div>
+
+              <div className="mt-4 text-center">
+                <div className="flex items-center justify-center">
+                  <div className="flex items-center justify-center">
+                    <a
+                      href="#"
+                      className="mx-2 font-semibold text-gray-700 dark:text-gray-200"
+                      tabindex="0"
+                      role="link"
+                    >
+                      {trainer.name}
+                    </a>
+                  </div>
+                  <span className="mx-1 text-xs text-gray-600 dark:text-gray-300">
+                    {trainer.location}
+                  </span>
+                
+
+                </div>
+              </div>
+              <div className="flex  justify-between items-center my-5">
+                  {
+                    trainer.social_media_links.map((link, index) => (
+                      <a
+                        key={index}
+                        href={link.url}
+                        className="mx-2 text-sky-400 dark:text-sky-300 hover:text-sky-500 dark:hover:text-sky-400 transition-colors duration-300"
+                        tabindex="0"
+                        role="link"
+                      >
+                        Social Media
+                      </a>
+                    ))
+                  }
+                  </div>
+            </div>
+          </div>
+          <div >
+            {
+                trainer.available_slots.map((slot, index) => (
+                    <div key={index} className="flex flex-col items-center justify-between w-full mt-4 bg-white rounded-lg shadow-md dark:bg-gray-800 my-2 gap-4 p-6" >
+                        <div>
+                        <span className="text-xs font-semibold text-blue-600 uppercase dark:text-blue-400">
+                            {slot.day}
+                        </span>
+                        <a
+                            href="#"
+                            className="block mt-2 text-xl font-semibold text-gray-800 transition-colors duration-300 transform dark:text-white hover:text-gray-600 hover:underline"
+                            tabindex="0"
+                            role="link"
+                        >
+                           {slot}
+                        </a>
+                       <p>
+                        {trainer.name} is available on {slot} .
+                       </p>
+                       <p>
+                        {trainer.email} is the email to contact {trainer.name} for booking.
+                       </p>
+                        </div>
+                        <div className="flex items-center justify-center">
+                       <Link to={`/book/${trainer._id}`} className="mx-2 font-semibold text-gray-700 dark:text-gray-200 btn my-5">
+                       <button
+                            
+                            className="bg-teal-300 px-3 py-3 rounded mx-2 font-semibold text-gray-700 dark:text-gray-200"
+                            
+                            
+                        >
+                            Book Now
+                        </button>
+                        </Link>
+                        </div>
+                    </div>
+                    ))
+            }
+          </div>
         </div>
       ) : (
-        <p>Loading...</p>
+        <p>
+            <span>Loading...</span>
+        </p>
       )}
     </div>
   );
--- src/Pages/Home/News/Newsletter.jsx
+++ src/Pages/Home/News/Newsletter.jsx
@@ -1,6 +1,50 @@
 import React from 'react';
+import UseAxiosCommon from '../../../hooks/UseAxiosCommon';
+import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import { useForm } from 'react-hook-form';
 
 const Newsletter = () => {
+    const axiosCommon=UseAxiosCommon();
+    const navigate=useNavigate();
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+        reset,
+      } = useForm();
+    
+    const onSubmit = (data) => {
+        const { email } = data;
+        const user={
+            email:email,
+            role:'member',
+          }
+          axiosCommon.post('newsletter',user)
+          .then((res)=>{
+            console.log(res.data)
+            if (res.data.insertedId) {
+              Swal.fire({
+                icon: "success",
+                title: "Congratulation",
+                text: "You will get regularly updates from us!",
+              });
+              reset();
+              navigate('/')
+            }else{
+                Swal.fire({
+                    icon: "error",
+                    title: "You have already subscribed!",
+                    text: `${res.data.message}`
+                });
+                
+            }
+        }
+        )
+        
+
+    }
     return (
         <div className='py-7'>
            <div className="w-full py-3 bg-gray-500 transition-all duration-300 rounded-lg cursor-pointer filter grayscale hover:grayscale-0" style={{backgroundImage: "url('https://i.ibb.co/5YyvYNn/pexels-pixabay-531880.jpg')", backgroundPosition: "center center", backgroundBlendMode: "multiply", backgroundSize: "cover"}}>
@@ -10,10 +54,10 @@ const Newsletter = () => {
             Get fitness tips and updates from our trainers 1-2 times a week.
             So you can stay on track with your fitness goals.
         </p>
-		<div className="flex flex-row">
-			<input type="text" placeholder="example@email.com" className="w-3/5 p-3 rounded-l-lg sm:w-2/3" />
-			<button type="button" className="w-2/5 p-3 font-semibold rounded-r-lg sm:w-1/3 bg-[#17ACAC] text-white ">Subscribe</button>
-		</div>
+		<form onSubmit={handleSubmit(onSubmit)} className="flex flex-row">
+			<input type="text"  {...register("email", { required: true })} placeholder="example@email.com" className="w-3/5 p-3 rounded-l-lg sm:w-2/3" />
+			<button type="submit" className="w-2/5 p-3 font-semibold rounded-r-lg sm:w-1/3 bg-[#17ACAC] text-white ">Subscribe</button>
+		</form>
 	</div>
 </div>
  
